28188e2bb6a87921dfa13a58e07bd64394d980cc
fremasadi <fremasadii@gmail.com>
2025-06-29 21:07:32 +0700

    feat

--- resources/views/filament/actions/view-marriage-documents.blade.php
+++ resources/views/filament/actions/view-marriage-documents.blade.php
@@ -12,10 +12,20 @@
     @endphp
 
     @foreach ($documents as $field => $label)
-        @if ($record->{$field})
+        @php
+            $files = is_array($record->{$field}) ? $record->{$field} : json_decode($record->{$field}, true);
+        @endphp
+
+        @if (!empty($files))
             <div>
-                <p class="font-semibold">{{ $label }}</p>
-                <img src="{{ Storage::url($record->{$field}) }}" class="rounded shadow-md max-w-full h-auto" />
+                <p class="font-semibold mb-2">{{ $label }}</p>
+                <div class="flex flex-wrap gap-4">
+                    @foreach ($files as $file)
+                        <a href="{{ Storage::url($file) }}" target="_blank">
+                            <img src="{{ Storage::url($file) }}" class="w-32 h-32 object-cover rounded shadow" alt="{{ $label }}">
+                        </a>
+                    @endforeach
+                </div>
             </div>
         @endif
     @endforeach
