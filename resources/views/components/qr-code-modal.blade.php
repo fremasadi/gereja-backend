a6d56af58003bec59c363227d12dc9c889246032
fremasadi <fremasadii@gmail.com>
2025-06-30 14:35:43 +0700

    feat

---
+++ resources/views/components/qr-code-modal.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center space-y-4">
+    <img src="{{ $qrCodeImage }}" alt="QR Code" class="w-48 h-48">
+    <p class="text-sm text-gray-600">Data ID: {{ $qrData }}</p>
+</div>
