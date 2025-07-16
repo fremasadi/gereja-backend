1cfaf616d88c27852ae8adf46a23dd123630a6a5
fremasadi <fremasadii@gmail.com>
2025-07-16 11:16:56 +0700

    feat

--- database/migrations/2025_07_16_040141_update_users_replace_age_with_birthdaydate.php
+++ database/migrations/2025_07_16_040141_update_users_replace_age_with_birthdaydate.php
@@ -11,7 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $table->dropColumn('age');
         $table->date('birthdaydate')->nullable()->after('phone');
     }
 
